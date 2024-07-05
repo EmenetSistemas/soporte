734a17adee03baf6c70dec04586a3dba6dbb855b
Adrián Villa <idgs.adrian.villa@gmail.com>
2024-07-05 14:59:09 -0600

    Se ajusta barra navbar para mostrar información sesión

--- front/src/app/admin/components/navbar/navbar.component.ts
+++ front/src/app/admin/components/navbar/navbar.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { DataService } from '../../services/data/data.service';
+import { Router } from '@angular/router';
+import { MensajesService } from '../../services/mensajes/mensajes.service';
+import { LoginService } from 'src/app/auth/services/login/login.service';
+import { UsuariosService } from 'src/app/auth/services/usuarios/usuarios.service';
 
 @Component({
 	selector: 'app-navbar',
@@ -7,17 +11,58 @@ import { DataService } from '../../services/data/data.service';
 	styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-	protected informacionUsuario: any = [];
+	protected informacionUsuario : any = [];
+	
+	constructor(
+		private dataService: DataService,
+		private apiUsuarios: UsuariosService,
+		private router: Router,
+		private mensajes: MensajesService,
+		private apiLogin: LoginService
+	) { }
 
-	constructor (
-		private dataService: DataService
-	) {}
-
-	protected prueba(): void {
-		this.dataService.claseSidebar = this.dataService.claseSidebar == '' ? 'toggle-sidebar' : '';
+	async ngOnInit(): Promise<void> {
+		await this.obtenerDatosUsuarios();
 	}
 
-	protected logout(): void {
+	obtenerDatosUsuarios(): void {
+		let token = localStorage.getItem('token_soporte');
+		if (token != undefined) {
+			this.apiUsuarios.obtenerInformacionUsuarioPorToken(token).subscribe(
+				respuesta => {
+					this.informacionUsuario = respuesta[0];
+				}, error => {
+					localStorage.removeItem('token_soporte');
+					this.router.navigate(['/login']);
+					this.mensajes.mensajeGenerico('Al parecer su sesión expiró, necesita volver a iniciar sesión', 'error');
+				}
+			)
+		}
+	}
 
+	logout(): void {
+		this.mensajes.mensajeConfirmacionCustom('¿Estás seguro de cerrar sesión?', 'question', 'Cerrar sesión').then(
+			res => {
+				if (!res.isConfirmed) return;
+
+				this.mensajes.mensajeEsperar();
+				let token = localStorage.getItem('token_soporte');
+				this.apiLogin.logout(token).subscribe(
+					respuesta => {
+						localStorage.removeItem('token_soporte');
+						this.router.navigate(['/login']);
+						this.mensajes.mensajeGenerico(respuesta.mensaje, 'info');
+					},
+		
+					error => {
+						this.mensajes.mensajeGenerico('error', 'error');
+					}
+				);
+			}
+		);
+	}
+
+	prueba(): void {
+		this.dataService.claseSidebar = this.dataService.claseSidebar == '' ? 'toggle-sidebar' : '';
 	}
 }
---
+++ front/src/app/auth/services/usuarios/usuarios.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { api } from 'src/environments/environment';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class UsuariosService {
+	constructor(
+		private http: HttpClient
+	) { }
+
+	public obtenerInformacionUsuarioPorToken(token: any): Observable<any> {
+		return this.http.post<any>(api + '/usuarios/obtenerInformacionUsuarioPorToken', { token });
+	}
+}
